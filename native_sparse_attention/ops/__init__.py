d770eea6346ecbef9c3f514501901927d281e8dc
Yentl Collin <yentlcollin@Yentls-MacBook-Air.local>
2026-03-25 17:56:48 +0100

    Fix ops/__init__.py: wrap parallel_nsa import in try/except (fla submodule optional)

--- native_sparse_attention/ops/__init__.py
+++ native_sparse_attention/ops/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .naive import naive_nsa
-from .parallel import parallel_nsa
+
+try:
+    from .parallel import parallel_nsa
+except Exception:
+    parallel_nsa = None
 
 __all__ = [
     'naive_nsa',
